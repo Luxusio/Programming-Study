1e5310999b869b2654f02b58f83bccf504c5512e
Luxusio <44326048+Luxusio@users.noreply.github.com>
2021-04-30 12:19:34 +0900

    Learn SpringBoot
    Spriong Data JPA
    AOP
    Inflearn course 김영한 - 스프링 입문 - 코드로 배우는 스프링 부트, 웹 MVC, DB 접근 기술 end

--- Backend/SpringBoot/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ Backend/SpringBoot/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,29 +1,29 @@
 package hello.hellospring;
 
+import hello.hellospring.aop.TimeTraceAop;
 import hello.hellospring.repository.*;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.persistence.EntityManager;
-import javax.sql.DataSource;
-
 @Configuration
 public class SpringConfig {
 
+    private final MemberRepository memberRepository;
+
     @Autowired
-    EntityManager em;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     @Bean
-    public MemberService memberService(MemberRepository memberRepository) {
+    public MemberService memberService() {
         return new MemberService(memberRepository);
     }
 
     @Bean
-    public MemberRepository memberRepository() {
-        return new JpaMemberRepository(em);
+    public TimeTraceAop getTimeTraceAop() {
+        return new TimeTraceAop();
     }
-
-
 }
