71ab65ab5c8281488aa1cd319050ce6140f009f9
Luxusio <44326048+Luxusio@users.noreply.github.com>
2021-07-05 14:48:15 +0900

    JPA: Create Entity classes

--- Backend/SpringBoot/jpashop/jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ Backend/SpringBoot/jpashop/jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -14,25 +14,25 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class MemberRepositoryTest {
 
-    @Autowired MemberRepository memberRepository;
-
-    @Test
-    @Transactional
-    public void testMember() throws Exception {
-        // given
-        Member member = new Member();
-        member.setUsername("memberA");
-
-        // when
-        Long saveId = memberRepository.save(member);
-        Member findMember = memberRepository.find(saveId);
-
-        // then
-        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
-        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
-        System.out.println("findMember == member: " + (findMember == member));
-
-    }
+//    @Autowired MemberRepository memberRepository;
+//
+//    @Test
+//    @Transactional
+//    public void testMember() throws Exception {
+//        // given
+//        Member member = new Member();
+//        member.setUsername("memberA");
+//
+//        // when
+//        Long saveId = memberRepository.save(member);
+//        Member findMember = memberRepository.find(saveId);
+//
+//        // then
+//        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+//        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+//        System.out.println("findMember == member: " + (findMember == member));
+//
+//    }
 
 
 
