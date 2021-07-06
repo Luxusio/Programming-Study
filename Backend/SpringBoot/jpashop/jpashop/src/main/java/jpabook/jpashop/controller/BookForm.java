222b2490f5a89edbe84d6a53ae51b24802b29dd3
Luxusio <44326048+Luxusio@users.noreply.github.com>
2021-07-06 11:03:23 +0900

    JPA: Create ItemUpdate view

--- Backend/SpringBoot/jpashop/jpashop/src/main/java/jpabook/jpashop/controller/BookForm.java
+++ Backend/SpringBoot/jpashop/jpashop/src/main/java/jpabook/jpashop/controller/BookForm.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class BookForm {
 
     private Long id;
+
     private String name;
     private int price;
     private int stockQuantity;
