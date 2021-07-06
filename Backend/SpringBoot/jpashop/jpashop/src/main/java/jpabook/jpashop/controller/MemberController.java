446b47c9aaf792c4e4d53c49b255fec4f3194d85
Luxusio <44326048+Luxusio@users.noreply.github.com>
2021-07-06 10:49:37 +0900

    JPA: Create view memberList, itemCreate, itemList

--- Backend/SpringBoot/jpashop/jpashop/src/main/java/jpabook/jpashop/controller/MemberController.java
+++ Backend/SpringBoot/jpashop/jpashop/src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -38,6 +39,14 @@ public class MemberController {
 
         memberService.join(member);
         return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        // better : change member to memberDto, and use that model to render view
+        model.addAttribute("members", members);
+        return "members/memberList.html";
     }
 
 }
