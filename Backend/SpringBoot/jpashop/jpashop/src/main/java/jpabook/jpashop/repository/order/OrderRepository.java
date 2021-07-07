9f5b29d602a8cf984f785ef0cbadbd6f2824ffdb
Luxusio <44326048+Luxusio@users.noreply.github.com>
2021-07-07 16:16:45 +0900

    JPA: Create inspect orders
    v1: get raw entity
    v2: convert to dto and return it
    v3 optimize with fetch join

--- Backend/SpringBoot/jpashop/jpashop/src/main/java/jpabook/jpashop/repository/order/OrderRepository.java
+++ Backend/SpringBoot/jpashop/jpashop/src/main/java/jpabook/jpashop/repository/order/OrderRepository.java
@@ -67,4 +67,14 @@ public class OrderRepository {
         ).getResultList();
     }
 
+    public List<Order> findAllWithItem() {
+        // paging impossible when join 1:n
+        return em.createQuery(
+                "select distinct o from Order o" +
+                        " join fetch o.member m" +
+                        " join fetch o.delivery d" +
+                        " join fetch o.orderItems oi" +
+                        " join fetch oi.item i", Order.class
+        ).getResultList();
+    }
 }
