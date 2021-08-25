22ee8d39866f463376401a50d0aa6aab702e021a
Luxusio <44326048+Luxusio@users.noreply.github.com>
2021-08-25 11:35:30 +0900

    Spring: Move validation logic to validator

--- Backend/Spring/validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ Backend/Spring/validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -24,6 +24,7 @@ import java.util.Map;
 public class ValidationItemControllerV2 {
 
     private final ItemRepository itemRepository;
+    private final ItemValidator itemValidator;
 
     @GetMapping
     public String items(Model model) {
@@ -156,7 +157,7 @@ public class ValidationItemControllerV2 {
         return "redirect:/validation/v2/items/{itemId}";
     }
 
-    @PostMapping("/add")
+//    @PostMapping("/add")
     public String addItemV4(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
 
         log.info("objectName={}", bindingResult.getObjectName());
@@ -169,7 +170,7 @@ public class ValidationItemControllerV2 {
 
         if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
             bindingResult.rejectValue("price", "range", new Object[] { 1000, 1000000 }, null);
-       }
+        }
 
         if (item.getQuantity() == null || item.getQuantity() >= 10000) {
             bindingResult.rejectValue("quantity", "max", new Object[] { 9999 }, null);
@@ -182,6 +183,27 @@ public class ValidationItemControllerV2 {
                 bindingResult.reject("totalPriceMin", new Object[] { 10000, resultPrice }, null);
             }
         }
+
+        // 에러 로직
+        if (bindingResult.hasErrors()) {
+            log.info("errors={}", bindingResult);
+            return "validation/v2/addForm";
+        }
+
+        // 성공 로직
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
+
+    @PostMapping("/add")
+    public String addItemV5(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+
+//        log.info("objectName={}", bindingResult.getObjectName());
+//        log.info("target={}", bindingResult.getTarget());
+
+        itemValidator.validate(item, bindingResult);
 
         // 에러 로직
         if (bindingResult.hasErrors()) {
