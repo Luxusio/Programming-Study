75c84a0ddb8c445caa4035cb5da83c8672dab78b
Luxusio <44326048+Luxusio@users.noreply.github.com>
2021-08-26 18:20:10 +0900

    Spring: Use servlet's session to impl login

--- Backend/Spring/login/src/main/java/hello/login/web/HomeController.java
+++ Backend/Spring/login/src/main/java/hello/login/web/HomeController.java
@@ -2,6 +2,7 @@ package hello.login.web;
 
 import hello.login.domain.member.Member;
 import hello.login.domain.member.MemberRepository;
+import hello.login.web.session.SessionConst;
 import hello.login.web.session.SessionManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -9,8 +10,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @Slf4j
 @Controller
@@ -40,6 +43,7 @@ public class HomeController {
         return "loginHome";
     }
 
+//    @GetMapping("/")
     public String homeLoginV2(HttpServletRequest request, Model model) {
         // 세션 관리자에 저장된 회원 정보 조회
         Member member = (Member) sessionManager.getSession(request);
@@ -49,6 +53,19 @@ public class HomeController {
         }
 
         model.addAttribute("member", member);
+        return "loginHome";
+    }
+
+    @GetMapping("/")
+    public String homeLoginV3(
+            @SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member loginMember,
+            Model model) {
+
+        if (loginMember == null) {
+            return "home";
+        }
+
+        model.addAttribute("member", loginMember);
         return "loginHome";
     }
 
---
+++ Backend/Spring/login/src/main/java/hello/login/web/session/SessionConst.java
@@ -0,0 +1,9 @@
+package hello.login.web.session;
+
+public interface SessionConst {
+
+    String LOGIN_MEMBER = "loginMember";
+
+
+
+}
