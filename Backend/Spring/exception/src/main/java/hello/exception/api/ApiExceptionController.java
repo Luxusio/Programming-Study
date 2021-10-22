b52409095aabf404314f3a530822e310d79e43bc
Luxusio <44326048+Luxusio@users.noreply.github.com>
2021-10-22 16:02:09 +0900

    Spring: Handle exception with HandlerExceptionResolver

--- Backend/Spring/exception/src/main/java/hello/exception/api/ApiExceptionController.java
+++ Backend/Spring/exception/src/main/java/hello/exception/api/ApiExceptionController.java
@@ -1,5 +1,6 @@
 package hello.exception.api;
 
+import hello.exception.exception.UserException;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
@@ -16,6 +17,12 @@ public class ApiExceptionController {
 
         if ("ex".equals(id)) {
             throw new RuntimeException("잘못된 사용자");
+        }
+        if ("bad".equals(id)) {
+            throw new IllegalArgumentException("잘못된 입력 값");
+        }
+        if ("user-ex".equals(id)) {
+            throw new UserException("사용자 오류");
         }
 
         return new MemberDto(id, "hello " + id);
