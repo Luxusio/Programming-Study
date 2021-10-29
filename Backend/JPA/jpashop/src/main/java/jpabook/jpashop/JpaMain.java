3f2404ced3bcf0e3ba258ce9279d2dff61b8a805
Luxusio <44326048+Luxusio@users.noreply.github.com>
2021-10-29 17:26:33 +0900

    JPA: Exam add more thing

--- Backend/JPA/jpashop/src/main/java/jpabook/jpashop/JpaMain.java
+++ Backend/JPA/jpashop/src/main/java/jpabook/jpashop/JpaMain.java
@@ -23,6 +23,11 @@ public class JpaMain {
             Order order = em.find(Order.class, 1L);
             Long memberId = order.getMemberId();
 
+            Member member = em.find(Member.class, memberId);
+            String memberName = member.getName();
+
+            System.out.println("memberName = " + memberName + ", orderStatus=" + order.getOrderStatus());
+
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
