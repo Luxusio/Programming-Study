6f075e2618d990ab7b88a6152d982e2668060b31
Luxusio <44326048+Luxusio@users.noreply.github.com>
2021-12-06 14:00:33 +0900

    Spring: Learn JDK Dynamic Proxy

--- Backend/Spring/proxy/src/test/java/hello/proxy/jdkdynamic/ReflectionTest.java
+++ Backend/Spring/proxy/src/test/java/hello/proxy/jdkdynamic/ReflectionTest.java
@@ -2,9 +2,6 @@ package hello.proxy.jdkdynamic;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.engine.descriptor.MethodBasedTestDescriptor;
-
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 @Slf4j
