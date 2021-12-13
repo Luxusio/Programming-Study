744b17cf5c3e094b2bf33853750e0782f9d2fd24
Luxusio <44326048+Luxusio@users.noreply.github.com>
2021-12-13 17:20:29 +0900

    Spring: Learn AutoProxyConfiguration

---
+++ Backend/Spring/proxy/src/main/java/hello/proxy/config/v5_autoproxy/AutoProxyConfig.java
@@ -0,0 +1,32 @@
+package hello.proxy.config.v5_autoproxy;
+
+import hello.proxy.config.AppV1Config;
+import hello.proxy.config.AppV2Config;
+import hello.proxy.config.v3_proxyfactory.advice.LogTraceAdvice;
+import hello.proxy.trace.logtrace.LogTrace;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.aop.Advisor;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+import org.springframework.aop.support.NameMatchMethodPointcut;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration
+@Slf4j
+@Import({AppV1Config.class, AppV2Config.class})
+public class AutoProxyConfig {
+
+    // 자동 프록시 생성기가 프록시 생성을 이미 다 담당하므로 Advisor 만 있으면 됨.
+    @Bean
+    public Advisor getAdvisor1(LogTrace logTrace) {
+        // pointcut
+        NameMatchMethodPointcut pointcut = new NameMatchMethodPointcut();
+        pointcut.setMappedNames("request*", "order*", "save*");
+        // advice
+        LogTraceAdvice advice = new LogTraceAdvice(logTrace);
+
+        return new DefaultPointcutAdvisor(pointcut, advice);
+    }
+
+}
